63e9d422e7a27ad14ade95b9367da0e98296281a
SHAISTA ABID <shaistaabid76@gmail.com>
2022-01-28 12:05:23 +0500

    validate email and phone number

--- app/commands/register_user.rb
+++ app/commands/register_user.rb
@@ -10,7 +10,8 @@ class RegisterUser
     end
   
     def call
-        if not user.present?
+
+        if user
             user = User.create!(name: @name, email: @email, password: @password, phone_number: @phone_number, company_name: @company_name)
             JsonWebToken.encode(user_id: user.id)
         end
@@ -21,9 +22,12 @@ class RegisterUser
     attr_accessor :email, :password
   
     def user
+        return errors.add :user_authentication, 'Invalid Credentials! Email, Phone Number and Password could not be empty.' if not @email.present? or not @phone_number.present? or not @password.present?
         user = User.find_by_email(email)
-        errors.add :user_authentication, 'User already exists!' if user.present?
-        user
+        return errors.add :user_authentication, 'User already exists!' if user.present?
+        user = User.find_by(phone_number: @phone_number)
+        return errors.add :user_authentication, 'Phone number is already taken!' if user.present? and @phone_number == user.phone_number
+        true
     end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     has_one_time_password 
     validates :email, uniqueness: true, presence: true
-    validates :phone_number, presence: true
+    validates :phone_number, uniqueness: true, presence: true
 end
