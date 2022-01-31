a6b7b3645b431ee6e3debf080fab7dba98d9a1e8
SHAISTA ABID <shaistaabid76@gmail.com>
2022-01-31 15:22:29 +0500

    forgot password and reset password API

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
     has_secure_password
     has_one_time_password 
+
+
+    def generate_password_token
+        token = SecureRandom.hex(10)
+        self.password_token = token
+        save!
+        token
+    end
+
+    def reset_password!(password)
+        update(password: password)
+    end
 end
