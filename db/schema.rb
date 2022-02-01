635106e48d437de0c136f11c13d156f0939ce595
SHAISTA ABID <shaistaabid76@gmail.com>
2022-02-01 20:01:00 +0500

    create gig table with attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_083346) do
+ActiveRecord::Schema.define(version: 2022_02_01_061834) do
+
+  create_table "gigs", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "amount"
+    t.integer "review_count"
+    t.string "average_star"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
