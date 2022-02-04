ec240b5d1b7503d48d527efd8e7422844d0e42ee
SHAISTA ABID <shaistaabid76@gmail.com>
2022-02-04 21:45:18 +0500

    refactor password controller code

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,53 +1,31 @@
 class PasswordsController < ApplicationController
     skip_before_action :authenticate_request
-    def forgot_password
-        if params[:email].blank?
-            return render json: {error: 'Email cannot be empty! '}, status: :unprocessable_entity
-        else
-            user = User.find_by_email(params[:email])
 
-            if user.present?
-                token = user.generate_password_token
-                send_reset_password_email(user, token)
-                render json: {message: 'A password reset token has been sent to your email.'}, status: :ok
-            else
-                return render json: {error: 'Your account with this email does not exist! Please try again'}, status: :not_found
-            end
-        
+    def forgot_password
+        @result =  Passwords::ForgotPassword.call(params: params)
+        if @result.success?
+            render json: { status: @result.message }, status: :ok
+        else
+            render json: {error: (@result.message)}, status: @result.status
         end
     end
 
     def reset_password
-        if params[:password_token].blank?
-            return render json: {error: 'Token cannot be null or empty! '}, status: :unprocessable_entity
+        @result =  Passwords::ResetPassword.call(params: params)
+        if @result.success?
+            render json: {
+                user: {
+                name: @result.user.name,
+                email: @result.user.email,
+                phone_number: @result.user.phone_number,
+                updated_at: @result.user.created_at.strftime("%d-%m-%Y, %z %H:%M:%S"),
+                status: @result.message
+                }
+            },status: :ok
         else
-            @user = User.find_by(password_token: params[:password_token])
-            if @user.present?
-                if not password_errors!
-                    @user.reset_password!(params[:password])
-                    render json: {message: 'Password changed successfully!'}, status: :ok
-                end
-            else
-                return render json: {error: 'Invalid token! Please try again!'}, status: :unprocessable_entity
-            end
+            render json: {error: (@result.message)}, status: @result.status
         end
 
-    end
-
-    private
-
-    def send_reset_password_email(user, token)
-        PasswordResetMailer.send_password_reset_email(user, token).deliver_now
-    end
-
-    def password_errors!
-        if params[:password].blank?
-            return render json: {error: 'Password cannot be blank! '}, status: :unprocessable_entity
-        else
-            if params[:password] != params[:password_confirmation]
-                return render json: {error: 'Password confirmation is not matched with your password! '}, status: :unprocessable_entity
-            end
-        end
     end
     
 end
