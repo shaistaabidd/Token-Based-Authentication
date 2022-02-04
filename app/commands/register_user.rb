96f855039d843c38aed0ca350699c0ab454de0dc
SHAISTA ABID <shaistaabid76@gmail.com>
2022-02-04 21:42:37 +0500

    change error message text and return token along with user on signup

--- app/commands/register_user.rb
+++ app/commands/register_user.rb
@@ -14,7 +14,7 @@ class RegisterUser
         if user
             if @password == @password_confirmation
                 user = User.create!(name: @name, email: @email, password: @password, phone_number: @phone_number, company_name: @company_name, password_confirmation: @password_confirmation)
-                JsonWebToken.encode(user_id: user.id)
+                return {token: JsonWebToken.encode(user_id: user.id), user: user}
             else
                 return errors.add :error, 'Password confirmation is not matched with your password. '
             end
@@ -28,7 +28,7 @@ class RegisterUser
   
     def user
         
-        return errors.add :user_authentication, 'Invalid Credentials! Email, Phone Number and Password could not be empty.' if not @email.present? or not @phone_number.present? or not @password.present?
+        return errors.add :user_authentication, 'Invalid Credentials! param email, phone_number and password cannot be empty.' if not @email.present? or not @phone_number.present? or not @password.present?
         user = User.find_by_email(email)
         return errors.add :user_authentication, 'User already exists!' if user.present?
         user = User.find_by(phone_number: @phone_number)
