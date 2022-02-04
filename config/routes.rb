e1681f919f81d0bc578feb12f342ff84c67d8565
SHAISTA ABID <shaistaabid76@gmail.com>
2022-02-04 21:50:22 +0500

    set routes for otp sign in and simple sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   get 'users/create'
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'otp_sign_in', to: 'authentication#otp_sign_in'
   post 'sign_in', to: 'authentication#sign_in'
   post 'sign_up', to: 'authentication#sign_up'
   patch 'update', to: 'profiles#update'
-  post 'verify_otp', to: 'authentication#verify_otp'
+  post 'verify_otp', to: 'otps#verify_otp'
   post 'forgot_password', to: 'passwords#forgot_password'
   post 'reset_password', to: 'passwords#reset_password'
   # post 'create_gig', to: 'gigs#create_gig'
